fdcd3de0868a440e260ff4d94120ff444a8af301
shantanu <shantanu@oliveglobal.com>
2019-05-15 16:39:39 +0530

    Hello Universe

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+// import axios from 'axios';
+
+// @connect((store)=>{
+//   return {};
+// })
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.contentWillMount();
+  }
+
+ render(){
+     return(
+         <div><h1>Hello----- Index</h1></div>
+     );
+ }
+
+}
+
+
+export default App;
